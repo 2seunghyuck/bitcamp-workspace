ddc2a28c4da08ce70aba8662db510ee5f7877b38
2seugnhyuck <rotid1818@gmail.com>
2020-07-24 19:45:06 +0900

    회원정보수정

--- bitcamp-java-project/src/main/java/com/eomcs/pms/App3.java
+++ bitcamp-java-project/src/main/java/com/eomcs/pms/App3.java
@@ -1,63 +1,85 @@
 package com.eomcs.pms;
 
+import java.util.Scanner;
+import java.sql.Date;
+
 public class App3 {
 
   public static void main(String[] args) {
-    java.util.Scanner keyInput = new java.util.Scanner(System.in);
+    Scanner keyInput = new Scanner(System.in);
+
+    final int MAX_LENGTH = 5;
+    String[] proj = new String[MAX_LENGTH];
+    int[] no = new int[MAX_LENGTH];
+    String[] text = new String[MAX_LENGTH];
+    Date[] end = new Date[MAX_LENGTH];
+    String[] stat = new String[MAX_LENGTH];
+    String[] engi = new String[MAX_LENGTH];
+    int count = 0;
+
     System.out.println("[작업]");
-    
-    System.out.print("프로젝트? ");
-    String proj = keyInput.nextLine();
-    
-    System.out.print("번호? ");
-    String str = keyInput.nextLine();
-    int no = Integer.parseInt(str);   //입력값을 문자로 받아서 숫자로 리턴해줌
 
-    System.out.print("내용? ");
-    String text = keyInput.nextLine();
-    
-    System.out.print("완료일? ");
-    java.sql.Date end = java.sql.Date.valueOf(keyInput.nextLine());
-    
-    System.out.println("상태? ");
-    System.out.println("0: 신규");
-    System.out.println("1: 진행중");
-    System.out.println("2: 완료");
-    System.out.print("> ");
-    String stat = keyInput.nextLine();
-    
-    System.out.print("담당자? ");
-    String engi = keyInput.nextLine();
-    keyInput.close();
-    
-    System.out.println("-------------");
-    System.out.printf("프로젝트: %s%n", proj);
-    System.out.printf("번호: %d%n", no);
-    System.out.printf("내용: %s%n", text);
-    System.out.printf("완료일: %s%n", end);
-    System.out.printf("상태: ");
+    for (int i = 0; i < MAX_LENGTH; i++) {
+      count++;
+      System.out.print("프로젝트? ");
 
-    //    if (stat.equals("0")) {
-    //      System.out.println("신규");
-    //    } else if (stat.equals("1")) {
-    //      System.out.println("진행중");
-    //    } else {
-    //      System.out.println("완료");
-    //    }
+      proj[i] = keyInput.nextLine();
 
-    switch (stat) {
-      case "0":
-        System.out.println("신규");
+      System.out.print("번호? ");
+      no[i] = keyInput.nextInt();
+      keyInput.nextLine(); // 입력값을 문자로 받아서 숫자로 리턴해줌
+
+      System.out.print("내용? ");
+      text[i] = keyInput.nextLine();
+
+      System.out.print("완료일? ");
+      end[i] = Date.valueOf(keyInput.nextLine());
+
+      System.out.println("상태? ");
+      System.out.println("0: 신규");
+      System.out.println("1: 진행중");
+      System.out.println("2: 완료");
+      System.out.print("> ");
+      stat[i] = keyInput.nextLine();
+
+      System.out.print("담당자? ");
+      engi[i] = keyInput.nextLine();
+      System.out.println("더 입력하겠습니까 ? (y/N)");
+      String response = keyInput.nextLine();
+      if (response.equalsIgnoreCase("y") == false) {
         break;
-      case "1":
-        System.out.println("진행중");
-        break;
-      default:
-        System.out.println("종료");
+      }
     }
-    
-    System.out.printf("담당자: %s%n", engi);
+    keyInput.close();
+
+    System.out.println("-------------");
+
+
+    // if (stat.equals("0")) {
+    // System.out.println("신규");
+    // } else if (stat.equals("1")) {
+    // System.out.println("진행중");
+    // } else {
+    // System.out.println("완료");
+    // }
+    for (int i = 0; i < MAX_LENGTH; i++) {
+      switch (stat[i]) {
+        case "0":
+          System.out.println("신규");
+          break;
+        case "1":
+          System.out.println("진행중");
+          break;
+        default:
+          System.out.println("종료");
+      }
+    }
+    for (int i = 0; i < count; i++) {
+      System.out.printf("%s, %d, %s, %s, %s, %s%n", proj[i], no[i], text[i], end[i], stat[i], engi[i]);
+    }
+
+
   }
 }
-//String 문자열입력을 받을 리터럴을 비교할때 [a == b] 말고 [a.equals("b")] 해야함.
+// String 문자열입력을 받을 리터럴을 비교할때 [a == b] 말고 [a.equals("b")] 해야함.
 
--- bitcamp-java-project/src/main/java/com/eomcs/pms/App_b.java
+++ bitcamp-java-project/src/main/java/com/eomcs/pms/App_b.java
@@ -1,18 +1,19 @@
 package com.eomcs.pms;
 
-//클래스의 패키지 정보를 미리선언하여 컴파일러에게 알려준다.
+// 클래스의 패키지 정보를 미리선언하여 컴파일러에게 알려준다.
 import java.util.Scanner;
 import java.sql.Date;
+
 // 1)낱개의 변수를 사용하여 여러 회원 정보 처리하기
 // 2) 조건문을 사용하여 입출력 제어하기
 // - 필요한 만큼만 입력 받고 출력하기.
 public class App_b {
 
   public static void main(String[] args) {
-    
+
     java.util.Scanner keyInput = new Scanner(System.in);
     System.out.println("[회원]");
-    
+
     // 최대 5명의 회원 정보를 입력받을 변수를 선언한다.
     int no1 = 0, no2 = 0, no3 = 0, no4 = 0, no5 = 0;
     String name1 = "", name2 = "", name3 = "", name4 = "", name5 = "";
@@ -22,41 +23,42 @@ public class App_b {
     String tell1 = "", tell2 = "", tell3 = "", tell4 = "", tell5 = "";
     Date now1 = null, now2 = null, now3 = null, now4 = null, now5 = null;
     long currentMillis;
+
     int count = 1;
     System.out.print("번호? ");
     no1 = keyInput.nextInt();
     keyInput.nextLine();
     System.out.print("이름? ");
-    name1 = keyInput.nextLine(); 
-    
+    name1 = keyInput.nextLine();
+
     System.out.print("이메일? ");
     email1 = keyInput.nextLine();
-    
+
     System.out.print("암호? ");
     password1 = keyInput.nextLine();
-    
+
     System.out.print("사진? ");
     photo1 = keyInput.nextLine();
 
     System.out.print("전화? ");
     tell1 = keyInput.nextLine();
-    
-    //가입일은 현재시간을 기준하기 때문에 기입하는 명령
+
+    // 가입일은 현재시간을 기준하기 때문에 기입하는 명령
     currentMillis = System.currentTimeMillis();
     now1 = new Date(currentMillis);
-    //String reg = keyInput.nextLine(); //현재시간을 나타내므로 직접 입력받을 필요가 없다.
-    
+    // String reg = keyInput.nextLine(); //현재시간을 나타내므로 직접 입력받을 필요가 없다.
+
     System.out.print("계속 입력하시겠습니까 ?(y/N) ");
     String response = keyInput.nextLine();
-    
-    if (response.equals("y") || response.equals("Y")) {
-      count ++;
-      //추가로 회원정보를 입력받는다.
+
+    if (response.equals("y") || response.equals("Y")) { 
+      count++;
+      // 추가로 회원정보를 입력받는다.
       System.out.print("번호? ");
       no2 = keyInput.nextInt();
       keyInput.nextLine();
       System.out.print("이름? ");
-      name2 = keyInput.nextLine(); 
+      name2 = keyInput.nextLine();
       System.out.print("이메일? ");
       email2 = keyInput.nextLine();
       System.out.print("암호? ");
@@ -68,90 +70,95 @@ public class App_b {
       currentMillis = System.currentTimeMillis();
       now2 = new Date(currentMillis);
       System.out.println("");
-    
+
       System.out.print("계속 입력하시겠습니까 ?(y/N) ");
       response = keyInput.nextLine();
+      if (response.equalsIgnoreCase("y")) {
+        count++;
+        // 3번 회원 입력
+        System.out.print("번호? ");
+        no3 = keyInput.nextInt();
+        keyInput.nextLine();
+        System.out.print("이름? ");
+        name3 = keyInput.nextLine();
+        System.out.print("이메일? ");
+        email3 = keyInput.nextLine();
+        System.out.print("암호? ");
+        password3 = keyInput.nextLine();
+        System.out.print("사진? ");
+        photo3 = keyInput.nextLine();
+        System.out.print("전화? ");
+        tell3 = keyInput.nextLine();
+        currentMillis = System.currentTimeMillis();
+        now3 = new Date(currentMillis);
+        System.out.println("");
+
+        System.out.print("계속 입력하시겠습니까 ?(y/N) ");
+        response = keyInput.nextLine();
         if (response.equalsIgnoreCase("y")) {
-          count ++;
-          // 3번 회원 입력
+          count++;
+          // 4번 회원 입력
           System.out.print("번호? ");
-          no3 = keyInput.nextInt();
+          no4 = keyInput.nextInt();
           keyInput.nextLine();
           System.out.print("이름? ");
-          name3 = keyInput.nextLine(); 
+          name4 = keyInput.nextLine();
           System.out.print("이메일? ");
-          email3 = keyInput.nextLine();
+          email4 = keyInput.nextLine();
           System.out.print("암호? ");
-          password3 = keyInput.nextLine();
+          password4 = keyInput.nextLine();
           System.out.print("사진? ");
-          photo3 = keyInput.nextLine();
+          photo4 = keyInput.nextLine();
           System.out.print("전화? ");
-          tell3 = keyInput.nextLine();
+          tell4 = keyInput.nextLine();
           currentMillis = System.currentTimeMillis();
-          now3 = new Date(currentMillis);
+          now4 = new Date(currentMillis);
           System.out.println("");
-        
           System.out.print("계속 입력하시겠습니까 ?(y/N) ");
           response = keyInput.nextLine();
-            if (response.equalsIgnoreCase("y")) {
-              count ++;
-              // 4번 회원 입력
-              System.out.print("번호? ");
-              no4 = keyInput.nextInt();
-              keyInput.nextLine();
-              System.out.print("이름? ");
-              name4 = keyInput.nextLine(); 
-              System.out.print("이메일? ");
-              email4 = keyInput.nextLine();
-              System.out.print("암호? ");
-              password4 = keyInput.nextLine();
-              System.out.print("사진? ");
-              photo4 = keyInput.nextLine();
-              System.out.print("전화? ");
-              tell4 = keyInput.nextLine();
-              currentMillis = System.currentTimeMillis();
-              now4 = new Date(currentMillis);
-              System.out.println("");
-              System.out.print("계속 입력하시겠습니까 ?(y/N) ");
-              response = keyInput.nextLine();
-              
-              if (response.equalsIgnoreCase("y")) {
-                count ++;
-                // 5번 회원 입력
-                System.out.print("번호? ");
-                no5 = keyInput.nextInt();
-                keyInput.nextLine();
-                System.out.print("이름? ");
-                name5 = keyInput.nextLine(); 
-                System.out.print("이메일? ");
-                email5 = keyInput.nextLine();
-                System.out.print("암호? ");
-                password5 = keyInput.nextLine();
-                System.out.print("사진? ");
-                photo5 = keyInput.nextLine();
-                System.out.print("전화? ");
-                tell5 = keyInput.nextLine();
-                currentMillis = System.currentTimeMillis();
-                now5 = new Date(currentMillis);
-                }
-              }
-            }
+
+          if (response.equalsIgnoreCase("y")) {
+            count++;
+            // 5번 회원 입력
+            System.out.print("번호? ");
+            no5 = keyInput.nextInt();
+            keyInput.nextLine();
+            System.out.print("이름? ");
+            name5 = keyInput.nextLine();
+            System.out.print("이메일? ");
+            email5 = keyInput.nextLine();
+            System.out.print("암호? ");
+            password5 = keyInput.nextLine();
+            System.out.print("사진? ");
+            photo5 = keyInput.nextLine();
+            System.out.print("전화? ");
+            tell5 = keyInput.nextLine();
+            currentMillis = System.currentTimeMillis();
+            now5 = new Date(currentMillis);
           }
-        
+        }
+      }
+    }
+
     System.out.println("");
-    
+
     keyInput.close();
-    
+
     System.out.println("---------------------");
-    System.out.printf("%d, %s, %s, %s, %s, %s%n", no1, name1, email1, password1, tell1, now1.toString());
+    System.out.printf("%d, %s, %s, %s, %s, %s, %s%n", no1, name1, email1, password1, photo1, tell1,
+        now1.toString());
     if (count > 1) {
-      System.out.printf("%d, %s, %s, %s, %s, %s%n", no2, name2, email2, password2, tell2, now2.toString());
+      System.out.printf("%d, %s, %s, %s, %s, %s, %s%n", no2, name2, email2, password2, photo2, tell2,
+          now2.toString());
       if (count > 2) {
-        System.out.printf("%d, %s, %s, %s, %s, %s%n", no3, name3, email3, password3, tell3, now3.toString());
+        System.out.printf("%d, %s, %s, %s, %s, %s, %s%n", no3, name3, email3, password3, photo3, tell3,
+            now3.toString());
         if (count > 3) {
-          System.out.printf("%d, %s, %s, %s, %s, %s%n", no4, name4, email4, password4, tell4, now4.toString());
+          System.out.printf("%d, %s, %s, %s, %s, %s, %s%n", no4, name4, email4, password4, photo4, tell4,
+              now4.toString());
           if (count > 4) {
-            System.out.printf("%d, %s, %s, %s, %s, %s%n", no5, name5, email5, password5, tell5, now5.toString());
+            System.out.printf("%d, %s, %s, %s, %s, %s, %s%n", no5, name5, email5, password5, photo5, tell5,
+                now5.toString());
           }
         }
       }
